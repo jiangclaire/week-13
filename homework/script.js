c0a29e6bee0649ffb4c1aa32ede4482a0849297e
Claire Jiang <clairejiang@Claires-MacBook-Pro-3.local>
2017-04-22 23:19:19 -0400

    Week 13 Submission.

--- homework/script.js
+++ homework/script.js
@@ -1,12 +1,12 @@
 // LIST ARRAY IS WHERE OUR DATA FOR THIS APPLICATION LIVES
 var listArray = [{
-        name: "Books to Read",
-        items: ["Hitchhiker's Guide to Galaxy", "Walden", "The Elephant, the Tiger, and the Cell Phone"]
-    },
-    {
-        name: "Groceries to Buy",
-        items: ["Milk", "Eggs", "Butter"]
-    }
+    name: "Books to Read",
+    items: ["Hitchhiker's Guide to Galaxy", "Walden", "The Elephant, the Tiger, and the Cell Phone"]
+},
+{
+    name: "Groceries to Buy",
+    items: ["Milk", "Eggs", "Butter"]
+}
 ];
 var selectedList = 0;
 var listDiv = document.getElementById("lists");
@@ -28,14 +28,10 @@ var addItemButton = document.getElementById("add-item-button");
 // 4. add "active" to selected list
 // 5. update items for selected div
 listDiv.addEventListener("click", function(e) {
-    // console.log(e.target);
-    var clickedListElement = e.target;
-    console.log(clickedListElement.dataset);
+    var clickedListElement = e.target || e.srcElement;
     selectedList = clickedListElement.dataset.index;
-    console.log("selectedList", selectedList);
-
     updateLists();
-    updateItemsForSelectedList(;
+    //updateItemsForSelectedList();
 });
 
 // ------------------------------------------------------
@@ -46,7 +42,7 @@ function updateLists() {
         listDiv.removeChild(listDiv.lastChild);
     }
 
-    listArray.forEach(function(list, index) {
+    listArray.forEach(function(list, i) {
         // Create an 'a' element
         var aElement = document.createElement("a");
         aElement.classList.add("list-group-item");
@@ -61,36 +57,35 @@ function updateLists() {
         aElement.appendChild(textNode);
 
         listDiv.appendChild(aElement);
-    );
-}
+        ;
+    })
 
-function updateItemsForSelectedList() {
-    while (itemDiv.hasChildNodes()) {
-        itemDiv.removeChild(itemDiv.lastChild);
+    function updateItemsForSelectedList() {
+        while (itemDiv.hasChildNodes()) {
+            itemDiv.removeChild(itemDiv.lastChild);
+        }
+        var listItemArray = listArray[selectedList].items;
+        listItemArray.forEach(function(item, i) {
+            // Populate the list-items div (the right div) wit respective list items
+            // - make a new 'a' element
+            var aElement = document.createElement("a");
+            // - add classes to its classList
+            aElement.classList.add("list-group-item");
+            aElement.classList.add("list-group-item-action");
+            // - set value of 'data-index' attribute to i
+            // - Create a textNode with item name
+            var textNode = document.createTextNode(item);
+            // - append textNode to the 'a' element
+            aElement.appendChild(textNode);
+            // console.log(aElement);
+            // - append 'a' element to the itemDiv
+            itemDiv.appendChild(aElement);
+        });
     }
 
-    var listItemArray = listArray[selectedList].items;
-    listItemArray.forEach(function(item, i) {
-        // Populate the list-items div (the right div) wit respective list items
-        // - make a new 'a' element
-        var aElement = document.createElement("a");
-        // - add classes to its classList
-        aElement.classList.add("list-group-item");
-        aElement.classList.add("list-group-item-action");
-        // - set value of 'data-index' attribute to i
-        // - Create a textNode with item name
-        var textNode = document.createTextNode(item);
-        // - append textNode to the 'a' element
-        aElement.appendChild(textNode);
-        // console.log(aElement);
-        // - append 'a' element to the itemDiv
-        itemDiv.appendChild(aElement);
-    });
+    //updateLists();
+    updateItemsForSelectedList();
 }
-
-updateLists();
-updateItemsForSelectedList();
-
 // ------------------------------------------------------
 // ADDING TO LIST
 // ------------------------------------------------------
@@ -123,7 +118,8 @@ addItemButton.addEventListener("click", function(e) {
     // - add it into itemArray
     itemArray.push(inputValue);
     // - update listItem div
-    updateItemsForSelectedList();
+    updateLists();
+    //updateItemsForSelectedList();
 });
 
 // ------------------------------------------------------
@@ -133,7 +129,7 @@ var buttonsArray = document.querySelectorAll(".popup-button");
 // querySelectorAll returns a DOMTokenList and not an Array (which includes methods like forEach)
 buttonsArray = Array.from(buttonsArray); // Conevrting DOMTokenList to an Array
 
-buttonArray.forEach(function(button) {
+buttonsArray.forEach(function(button) {
     button.addEventListener("click", function() {
         var popup = document.getElementById(this.dataset.popupid);
         // The data attributes can be accessed by .dataset variable which is part of the DOMElement (check HTML for buttonsArray)
@@ -152,3 +148,4 @@ function closePopups() {
         popup.style.display = "none";
     });
 }
+
---
+++ task-1/script.js
@@ -0,0 +1,37 @@
+var array; //DEFINITION or DECLARATION
+array = ["a", "b", undefined, "c"];
+var string = "this is a string"; //INITIALIZATION 
+
+console.log(array);
+console.log(string);
+var randomName;
+
+var obj = { name: "Claire", 
+university: "Northeastern",
+forEach: function() {}
+}; //"name" is the KEY and "Claire" is the VALUE
+
+console.log(obj.forEach());
+
+var x = array[4];
+//console.log(x);
+//x.functionName();
+
+//console.log("a", "b", "30", "sakldf", [8, 62, 76]);
+
+var object = { odd: [1,3,5,7,9] };
+console.log(object.odd);
+
+//object.even.push(2);
+//object.odd.push(11); //SEMICOLON is a "full stop" -- if you don't have it then "unexpected token"
+
+//object.odd
+
+array.forEach(function() {
+
+});
+
+function functionName() {
+
+}
+
